f1802b50ee5455af7fa1c4417abdb2c96f13ff22
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-26 09:01:07 -0500

    p

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Parr extends Component {
-  render() {
-    if (!this.props.show) {
-      return null;
-    }
-    else {
-      return (<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>);
-    }
-  }
-}
-
 class App extends Component {
-  constructor() {
+  constructor(){
     super();
     this.state = {
-      show: false
+      show : false
     }
-  }
+  } 
   render() {
+    const parr = <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     return (
       <div className="wrapper">
-        <label><input type="checkbox" checked={this.state.show} onChange={this.changeState.bind(this)} /> Mostrar información importante</label>
-        <Parr show={this.state.show} />
+        <label><input type="checkbox" checked ={this.state.show} onChange={this.changeState.bind(this)}/> Mostrar información importante</label>
+        {this.state.show ? parr : null}
       </div>
     );
   }
-  changeState(event) {
+  changeState(event){
     this.setState({
       show: event.target.checked
     });
